4beb510b84d0ab199e0396eb2f0c4fb36e7deaf6
jawad <jawadali20017@gmail.com>
2022-11-08 12:05:17 +0500

    first commit

---
+++ resources/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>Wellcome</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+name:"wellcome",
+data(){
+return {}
+},
+}
+</script>
---
+++ resources/components/category/Edit.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Update Category</h4>
+                </div>
+                <div class="card-body">
+                    <form @submit.prevent="update">
+                        <div class="row">
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" v-model="category.name">
+                                </div>
+                            </div>
+                            <div class="col-12 mb-2">
+                                <div class="form-group">
+                                    <label>Slug</label>
+                                    <input type="text" class="form-control" v-model="category.slug">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"update-category",
+    data(){
+        return {
+            category:{
+                name:"",
+                slug:"",
+                _method:"patch"
+            }
+        }
+    },
+    mounted(){
+        this.showCategory()
+    },
+    methods:{
+        async showCategory(){
+            await this.axios.get(`/api/category/${this.$route.params.id}`).then(response=>{
+                const { name, slug } = response.data
+                this.category.name = name
+                this.category.slug = slug
+            }).catch(error=>{
+                console.log(error)
+            })
+        },
+        async update(){
+            await this.axios.post(`/api/category/${this.$route.params.id}`,this.category).then(response=>{
+                this.$router.push({name:"categoryList"})
+            }).catch(error=>{
+                console.log(error)
+            })
+        }
+    }
+}
+</script>
